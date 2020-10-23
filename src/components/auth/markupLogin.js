592fb9a1c18adb0b078b471746ff04db38c0c32c
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-24 00:11:31 +0300

    ready logics

---
+++ src/components/auth/markupLogin.js
@@ -0,0 +1,8 @@
+import markup from './markupLogin.hbs'
+
+function markupLogin(label) {
+  return markup(label);
+}
+
+
+export default markupLogin;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './scss/main.scss';
 import fetchExchangeRates from './components/serviceApi/fetchExchangeRates';
-import authWithEmailAndPassword from './components/auth/authApi'
+import { authWithEmailAndPassword, registrationWithEmailAndPassword } from './components/auth/authApi'
+import markupLogin from './components/auth/markupLogin'
+
+console.log(authWithEmailAndPassword);
+console.log(registrationWithEmailAndPassword);
 
 
-const email = 'test@email.com';
-const password = 'testemail';
+
+const loginBtnRef = document.querySelector('#login-btn');
+const regBtnRef = document.querySelector('#registration-btn');
+const rootAuthRef = document.querySelector('#root-auth')
 
 
 fetchExchangeRates.requestForTheCurrentDate().then(data => console.log(data));
@@ -13,8 +19,53 @@ fetchExchangeRates.requestForTheSelectedDate(20201011).then(data => console.log(
 
 
 
-authWithEmailAndPassword(email, password)
-  .then(({ message, registered }) => {
-    console.log('message', message);
-    console.log('registered', registered);
-  }).catch((error) => console.log('error', error));
+
+
+function authHandler(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  authWithEmailAndPassword(email.value, password.value)
+    .then(({ message, authorized }) => {
+      alert(message);
+      if (authorized) {
+        // formRef.removeEventListener('submit', formHandler);
+        rootAuthRef.innerHTML = '';
+        // СЮДА ВСТАВИТЬ ПОСЛЕ УСПЕШНОГО ВХОДА
+      }
+    })
+    .catch((error) => console.log('error', error));
+}
+
+function registrationHandler(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  registrationWithEmailAndPassword(email.value, password.value)
+    .then(({ message, registered }) => {
+      alert(message);
+      if (registered) {
+        // formRef.removeEventListener('submit', formHandler);
+        rootAuthRef.innerHTML = '';
+        // СЮДА ВСТАВИТЬ ПОСЛЕ УСПЕШНОЙ РЕГИСТРАЦИИ
+      }
+    })
+    .catch((error) => console.log('error', error));
+}
+
+function loginBtnHolder() {
+  rootAuthRef.innerHTML = markupLogin({ label: 'Login' });
+  const formRef = document.querySelector('form');
+  formRef.addEventListener('submit', authHandler);
+}
+
+function registrationBtnHolder() {
+  rootAuthRef.innerHTML = markupLogin({ label: 'Registration' });
+  const formRef = document.querySelector('form');
+  formRef.addEventListener('submit', registrationHandler);
+}
+
+loginBtnRef.addEventListener('click', loginBtnHolder);
+regBtnRef.addEventListener('click', registrationBtnHolder);
+
+
