bdf77da3035ad26430693d00d23969e34295659b
ArturStahov <vtemy.at.ua@gmail.com>
2020-10-24 01:03:28 +0300

    add base template and modal plugin

--- src/components/modal-window/modal-window-plugin.js
+++ src/components/modal-window/modal-window-plugin.js
@@ -5,6 +5,8 @@ export class ModalWindowPlugin {
         this.screenNavRef = document.querySelector(selectorScreenNav)
         this.modalTitle = this.modalRef.querySelector('[data-type="modal-title"]')
         this.buttonCloseModalRef = this.modalRef.querySelector('[data-type="modal-button-close"]')
+        this.buttonSubmit = this.modalRef.querySelector('#modal-form-button')
+
         this._bindEvent();
     }
     _bindEvent() {
@@ -14,10 +16,31 @@ export class ModalWindowPlugin {
 
     _handlerScreenNav(event) {
         let type = event.target.dataset.type;
-        if (event.nodeName !== 'BUTTON') {
-            return
+
+        if (type === 'button-login') {
+            this.modalTitle.textContent = 'log in'
+            this.buttonSubmit.dataset.type = 'login-btn'
+            this.openModal();
         }
-        console.log(type)
+        if (type === 'button-signin') {
+            this.modalTitle.textContent = 'sign in'
+            this.buttonSubmit.dataset.type = 'registration-btn'
+            this.openModal();
+
+        }
+        return
+    }
+    openModal() {
+        this.modalRef.classList.remove('modal-close')
+        this.modalRef.classList.add('modal-open')
+    }
+    closeModal() {
+        this.modalRef.classList.remove('modal-open')
+        this.modalRef.classList.add('modal-close')
+    }
+
+    _handlerButtonCloseModal() {
+        this.closeModal()
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './scss/main.scss';
 import fetchExchangeRates from './components/serviceApi/fetchExchangeRates';
 import authWithEmailAndPassword from './components/auth/authApi'
+import { ModalWindowPlugin } from './components/modal-window/modal-window-plugin.js'
 
 
 const email = 'test@email.com';
@@ -18,3 +19,11 @@ authWithEmailAndPassword(email, password)
     console.log('message', message);
     console.log('registered', registered);
   }).catch((error) => console.log('error', error));
+
+
+
+const modalOptions = {
+  selectorModal: '[data-type="modal-window"]',
+  selectorScreenNav: '[data-type="screen-nav"]'
+}
+const modalWindow = new ModalWindowPlugin(modalOptions)
