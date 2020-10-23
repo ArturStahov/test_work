570aed10f92de40843706568f68db28ad0076e88
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-23 17:19:03 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import authWithEmailAndPassword from './components/Auth/Auth'
+
+
+const email = 'test@email.com';
+const password = 'testemail';
+
+
+authWithEmailAndPassword(email, password)
+         .then(token => {
+
+            console.log(token);
+      })
