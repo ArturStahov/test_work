abe3a650ee44634e0222e6e91a740a5ec7b86377
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-24 22:26:41 +0300

    pnotify

--- src/components/auth/registrationHandlers.js
+++ src/components/auth/registrationHandlers.js
@@ -1,5 +1,6 @@
 import { ModalWindowPlugin } from '../modal-window/modal-window-plugin'
 import { authWithEmailAndPassword, registrationWithEmailAndPassword } from './authApi'
+import { success, error } from '../pnonify/pnotify';
 
 const splashRef = document.querySelector('[data-type="splash-screen"]');
 const mainScreenRef = document.querySelector('[data-type="main-screen"]');
@@ -26,10 +27,12 @@ export default function authHandler(event) {
     authWithEmailAndPassword(email.value, password.value)
       .then(({ message, authorized, email }) => {
         activeUser = { message, authorized, email }
-        alert(message);
         if (authorized) {
           activeUser = { message, authorized, email }
-
+          success({
+            title: 'You logged in successfully! ',
+            text: message,
+          });
           // СЮДА ВСТАВИТЬ ПОСЛЕ УСПЕШНОГО ВХОДА
           splashRef.classList.add('visually-hidden');
           modalWindow.closeModal();
@@ -38,18 +41,29 @@ export default function authHandler(event) {
           pageMainRef.classList.remove('splash-screen-bg');
         }
         if (!authorized) {
+          error({
+            title: 'Error! ',
+            text: message,
+          });
           form.reset();
         }
       })
-      .catch((error) => console.log('error', error))
+      .catch(err => {
+        error({
+          title: 'ERROR!',
+          text: err,
+        });
+      });
 
   button.dataset.type === 'registration-btn' &&
     registrationWithEmailAndPassword(email.value, password.value)
       .then(({ message, registered, email }) => {
-        alert(message);
         if (registered) {
           activeUser = { message, authorized: registered, email }
-
+          success({
+            title: 'You signed in successfully! ',
+            text: message,
+          });
           // СЮДА ВСТАВИТЬ ПОСЛЕ УСПЕШНОЙ РЕГИСТРАЦИИ
           splashRef.classList.add('visually-hidden');
           modalWindow.closeModal();
@@ -58,10 +72,19 @@ export default function authHandler(event) {
           pageMainRef.classList.remove('splash-screen-bg');
         }
         if (!registered) {
+          error({
+            title: 'Error! ',
+            text: message,
+          });
           form.reset();
         }
       })
-      .catch((error) => console.log('error', error))
+      .catch(err => {
+        error({
+          title: 'ERROR!',
+          text: err,
+        });
+      });
 }
 
 
