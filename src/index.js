d4c2f9b462821020e72ebe1eed94c781fbbc2063
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-25 23:06:30 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './scss/main.scss';
 // import fetchExchangeRates from './components/serviceApi/fetchExchangeRates';
-import authHandler from './components/auth/registrationHandlers';
+import { authHandler, logoutHandler } from './components/auth/registrationHandlers';
 import './components/chart/chart.js';
 import './components/exchangeRate/exchangeRate';
 const formLoginRef = document.querySelector('[data-type="modal-form"]');
-
+const btnLogoutRef = document.querySelector('[data-type="button-logout"]');
 
 
 
